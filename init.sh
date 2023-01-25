f132efea52c4d09a25239524f247e7fb63539a99
Vssblt <sunruiyangcp01@gmail.com>
2023-01-25 18:06:54 +0800

    change shadowsocks

--- init.sh
+++ init.sh
@@ -34,7 +34,7 @@ fi
 # diff-so-fancy # git diff beauty
 # 
 
-sudo su - $1 -c 'git submodule update --init'
+sudo runuser -u $1 -- git submodule update --init
 
 ubuntu_codename=`lsb_release -a | grep Codename -i | awk '{print $2}'`
 sudo wget https://mirrors.ustc.edu.cn/repogen/conf/ubuntu-https-4-$ubuntu_codename -O /etc/apt/sources.list
@@ -45,14 +45,15 @@ sudo apt update && sudo apt install git iputils-ping gcc g++ autoconf automake l
   openssh-server wget vim alsa-utils apt-transport-https gdb gdbserver xorg zsh \
   curl sudo libdigest-sha-perl psmisc net-tools iftop dnsutils telnet nfs-common \
   software-properties-common thunderbird rclone filezilla fzf ranger cpu-x neofetch \
-  google-chrome-stable firefox vlc xfce4-screenshooter gimp ffmpeg obs-studio \
+  firefox vlc xfce4-screenshooter gimp ffmpeg obs-studio \
   gnome-system-monitor figlet nodejs npm -y
 
 sudo dpkg -i ./packages/netease-cloud-music.deb
 
 echo "############################"
 echo "开始安装shadowsocks-libev"
-sudo su - $1 -c 'cd ./shadowsocks-libev_configuration/ && ./init.sh --setup && cd -'
+
+cd ./shadowsocks-libev_configuration/ && sudo bash ./init.sh --setup && cd - 
 
 echo "############################"
 echo "开始安装驱动"
@@ -60,29 +61,29 @@ sudo ubuntu-drivers autoinstall
 
 echo "############################"
 echo "开始安装docker和nvidia-docker"
-sudo su - $1 -c 'cd ./docker-install-shell && sudo bash install-docker.sh && cd -'
+sudo runuser -u $1 -- cd ./docker-install-shell && sudo bash install-docker.sh && cd -
 
 echo "############################"
 echo "开始安装i3"
 
 echo "############################"
 echo "开始安装polybar"
-sudo su - $1 -c 'cd ./polybar && ./init.sh && cd -'
+sudo runuser -u $1 -- cd ./polybar && ./init.sh && cd -
 
 echo "############################"
 echo "开始安装node相关软件"
-sudo npm install diff-so-fancy -y
-sudo su - $1 -c 'git config --global core.pager "diff-so-fancy | less --tabs=4 -RFX"'
-sudo su - $1 -c 'git config --global interactive.diffFilter "diff-so-fancy --patch"'
-sudo su - $1 -c 'git config --global color.ui true'
-sudo su - $1 -c 'git config --global color.diff-highlight.oldNormal    "red bold"'
-sudo su - $1 -c 'git config --global color.diff-highlight.oldHighlight "red bold 52"'
-sudo su - $1 -c 'git config --global color.diff-highlight.newNormal    "green bold"'
-sudo su - $1 -c 'git config --global color.diff-highlight.newHighlight "green bold 22"'
-sudo su - $1 -c 'git config --global color.diff.meta       "11"'
-sudo su - $1 -c 'git config --global color.diff.frag       "magenta bold"'
-sudo su - $1 -c 'git config --global color.diff.func       "146 bold"'
-sudo su - $1 -c 'git config --global color.diff.commit     "yellow bold"'
-sudo su - $1 -c 'git config --global color.diff.old        "red bold"'
-sudo su - $1 -c 'git config --global color.diff.new        "green bold"'
-sudo su - $1 -c 'git config --global color.diff.whitespace "red reverse"'
+sudo npm -g install diff-so-fancy -y
+sudo runuser -u $1 -- git config --global core.pager "diff-so-fancy | less --tabs=4 -RFX"
+sudo runuser -u $1 -- git config --global interactive.diffFilter "diff-so-fancy --patch"
+sudo runuser -u $1 -- git config --global color.ui true
+sudo runuser -u $1 -- git config --global color.diff-highlight.oldNormal    "red bold"
+sudo runuser -u $1 -- git config --global color.diff-highlight.oldHighlight "red bold 52"
+sudo runuser -u $1 -- git config --global color.diff-highlight.newNormal    "green bold"
+sudo runuser -u $1 -- git config --global color.diff-highlight.newHighlight "green bold 22"
+sudo runuser -u $1 -- git config --global color.diff.meta       "11"
+sudo runuser -u $1 -- git config --global color.diff.frag       "magenta bold"
+sudo runuser -u $1 -- git config --global color.diff.func       "146 bold"
+sudo runuser -u $1 -- git config --global color.diff.commit     "yellow bold"
+sudo runuser -u $1 -- git config --global color.diff.old        "red bold"
+sudo runuser -u $1 -- git config --global color.diff.new        "green bold"
+sudo runuser -u $1 -- git config --global color.diff.whitespace "red reverse"
